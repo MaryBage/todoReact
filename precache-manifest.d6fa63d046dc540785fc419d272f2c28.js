418c3c20ae53ee8bc7843de0c2eff35ce1471fb6
MaryBage <mary.bage.web@gmail.com>
2020-07-29 12:00:16 +0400

    Updates

---
+++ precache-manifest.d6fa63d046dc540785fc419d272f2c28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef019a4cb39e1b7ef4330dbfce3b80c",
+    "url": "/todoReact/index.html"
+  },
+  {
+    "revision": "7b6a9c9ee2e8192c72b7",
+    "url": "/todoReact/static/css/main.bd1c4bc5.chunk.css"
+  },
+  {
+    "revision": "24e1f518fd3289a08c77",
+    "url": "/todoReact/static/js/2.deb274c2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoReact/static/js/2.deb274c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b6a9c9ee2e8192c72b7",
+    "url": "/todoReact/static/js/main.543576cc.chunk.js"
+  },
+  {
+    "revision": "ad490fe27e402da2a67a",
+    "url": "/todoReact/static/js/runtime-main.637fa85f.js"
+  }
+]);
